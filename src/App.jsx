1ace4c943ac7cb4499f5a65cb6ceb6ac08a5238d
alwaysPasindu <always.pasindu@gmail.com>
2025-01-05 05:33:35 +0530

    re-modified the main files due to an error

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Properties from './pages/Properties'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import PropertyDetails from './pages/PropertyDetails'
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/properties" element={<Properties />} />
+            <Route path="/properties/:id" element={<PropertyDetails />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
