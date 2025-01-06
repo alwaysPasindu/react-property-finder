8e12f053b78b38fd553d89de2691411efbf56bd2
alwaysPasindu <always.pasindu@gmail.com>
2025-01-07 02:13:06 +0530

    deployment error fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,10 +29,10 @@ const Footer = () => {
         <div className="footer-section">
           <h3>Follow Us</h3>
           <div className="social-links">
-            <a href="#"><FaFacebook /></a>
-            <a href="#"><FaTwitter /></a>
-            <a href="#"><FaInstagram /></a>
-            <a href="#"><FaLinkedin /></a>
+            <a href="https://www.facebook.com/profile.php?id=100022030594134&mibextid=ZbWKwL" target="_blank"><FaFacebook /></a>
+            <a href="https://x.com/alwaysPasindu?s=09&mx=2" target="_blank"><FaTwitter /></a>
+            <a href="https://www.instagram.com/always.pasindu" target="_blank"><FaInstagram /></a>
+            <a href="https://www.linkedin.com/in/alwayspasindu/" target="_blank"><FaLinkedin /></a>
           </div>
         </div>
       </div>
